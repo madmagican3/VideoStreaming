591d76b7facfd1a2cfa3503064df72ed19349749
andrew beaumont <madmagican3@hambies.com>
2018-05-30 14:13:23 +0100

    Added auth control for later implementation + worked on layout

---
+++ Models/VideoModel.js
@@ -0,0 +1,21 @@
+/**
+ * This defines a video object and gives references to essential vars
+ * @param title The title of the video
+ * @param desc The description of the video
+ * @param loc The location of the video in the system , i.e. 'Test.mp4'
+ * @param tags The tags in the video for easier searching
+ * @param views The number of views
+ * @param up The number of upvotes
+ * @param de The number of downvotes
+ * @constructor This defines the video
+ */
+function Video(title, desc, loc,tags,views,up,de){
+    this.Title = title;
+    this.Description = desc;
+    this.location = loc;
+    this.tags = tags;
+    this.views = views;
+    this.upvotes = up;
+    this.devotes = de;
+}
+module.exports = Video;
--- routes/VideoTest.js
+++ routes/VideoTest.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(req,res,next){
+    //add cookie system to limit access to videos
+    next();
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('videoTest');
